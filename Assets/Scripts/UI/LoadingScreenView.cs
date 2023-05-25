f08561966bb70c7240eb79028f98d56279eabfef
Dmitriy <inoneair@gmail.com>
2023-05-25 19:54:06 +0500

    improve loading screen

--- Assets/Scripts/UI/LoadingScreenView.cs
+++ Assets/Scripts/UI/LoadingScreenView.cs
@@ -1,86 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-using DG.Tweening;
-using DG.Tweening.Core;
 using TMPro;
 
 public class LoadingScreenView : MonoBehaviour
 {
-    public enum State
-    {
-        Loading,
-        Complete
-    }
-
-    [SerializeField] private CanvasGroup _canvasGroup;
-    [SerializeField] private RectTransform _loadingImage;
+    [SerializeField] private ProcessMarker _marker;
     [SerializeField] private TMP_Text _text;
     [SerializeField] private string _loadingText;
     [SerializeField] private string _loadingCompleteText;
-    private bool _isSwitchOn = false;
 
-    private TweenerCore<float, float, DG.Tweening.Plugins.Options.FloatOptions> _fadeTweener;
-    private TweenerCore<Quaternion, Vector3, DG.Tweening.Plugins.Options.QuaternionOptions> _loadingImageTweener;
+    private void Awake()
+    {
+        SwitchOff();
+    }
 
     public void SwitchOn()
     {
-        if (_isSwitchOn)
-            return;
+        _text.text = _loadingText;
+        _marker.SwitchOn();
+    }
 
-        _isSwitchOn = true;
-        _canvasGroup.gameObject.SetActive(true);
-        _canvasGroup.alpha = 1.0f;
+    public async Task SwitchOnWithAnimation()
+    {
+        _text.text = _loadingText;
+        await _marker.SwitchOnWithAnimation();
     }
 
     public void SwitchOff()
     {
-        _isSwitchOn = false;
-        _canvasGroup.alpha = 0;
-        _canvasGroup.gameObject.SetActive(false);
+        _text.text = _loadingCompleteText;
+        _marker.SwitchOff();
     }
 
     public async Task SwitchOffWithAnimation()
     {
-        if (!_isSwitchOn)
-            return;
-
-        _isSwitchOn = false;
-
-        _fadeTweener = _canvasGroup.DOFade(0, 1f);
-        await _fadeTweener.AsyncWaitForCompletion();
-        _canvasGroup.gameObject.SetActive(false);
+        _text.text = _loadingCompleteText;
+        await _marker.SwitchOffWithAnimation();
     }
-
-    public void SetState(State state)
-    {
-        switch (state)
-        {
-            case State.Loading:
-                _text.text = _loadingText;
-                StartLoadingImageAnimation();
-                break;
-
-            case State.Complete:
-                _text.text = _loadingCompleteText;
-                StopLoadingImageAnimation();
-                break;
-        }
-    }
-
-    private void StartLoadingImageAnimation()
-    {
-        _loadingImageTweener ??= _loadingImage.DORotate(new Vector3(0, 0, 360), 1f, RotateMode.WorldAxisAdd).SetLoops(-1);        
-    }
-
-    private void StopLoadingImageAnimation()
-    {
-        if (_loadingImageTweener != null)
-        {
-            _loadingImageTweener.Complete();
-            _loadingImageTweener = null;
-        }
-    }
-
 }
